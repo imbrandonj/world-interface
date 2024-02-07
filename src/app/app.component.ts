826dc4b0d18b1933a3f0b60884ed23db3bc4d6d3
Brandon <imbrandonj42@gmail.com>
2024-02-07 17:13:09 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserSearchComponent } from './user-search/user-search.component';
+import { WorldMapComponent } from './world-map/world-map.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserSearchComponent, WorldMapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'world-interface';
+  title = 'World Interface';
 }
