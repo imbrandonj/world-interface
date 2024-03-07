257bd0d5ae4869cd8052fd11589258fa68d2282a
Brandon <imbrandonj42@gmail.com>
2024-03-07 14:12:29 -0600

    implements 2 new svg maps, formatting & styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserSearchComponent } from './user-search/user-search.component';
 import { WorldMapComponent } from './world-map/world-map.component';
+import { CountryInfoComponent } from './country-info/country-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserSearchComponent, WorldMapComponent],
+  imports: [RouterOutlet, UserSearchComponent, WorldMapComponent, CountryInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
