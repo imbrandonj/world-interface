23974afa3ed8d783e2d8c025b5d1314693f1ce28
Brandon <imbrandonj42@gmail.com>
2024-03-12 17:10:28 -0500

    implements both geonames & world bank api calls

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,34 +9,48 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
   
-  fetchCountryInfo(country: string) { // `country` arg is a 2 char country code
-    let api = `https://api.worldbank.org/v2/country/${country}?format=json`
+  // World Bank API
+  fetchWBCountryInfo(country: string) { // `country` arg is a 2 char country code
+    let api = `https://api.worldbank.org/v2/country/${country}?format=json`;
 
     return this.http.get(api)
   }
 
+  // GeoNames API
+  fetchGNCountryInfo(country: string) { // `country` arg is a 2 char country code
+    let api = `http://api.geonames.org/countryInfoJSON?country=${country}&username=imbrandonj`;
+
+    return this.http.get(api);
+  }
+
+
+  // method to set `subject` country info object making 2 api calls
   setCountryInfo(country: string) {  // `country` arg is a 2 char country code
     let subject = new Subject();
 
-    // calling the api and setting the country info:
-    this.fetchCountryInfo(country).subscribe((data: any) => {
-      try {
-      subject.next({
-        country: data[1][0].name,
-        capital: 'Capital: ' + data[1][0].capitalCity,
-        region: 'Region: ' + data[1][0].region.value,
-        income: 'Income: ' + data[1][0].incomeLevel.value,
-      })}
+    // calling the GeoNames API to set country, capital, population:
+    this.fetchGNCountryInfo(country).subscribe((geoData: any) => {
 
-      // api does not contain information:
-      catch (error) {
-        subject.next({
-        country: 'Country: Unknown',
-        capital: 'Capital: No Data',
-        region: 'Region: No Data',
-        income: 'Income: No Data',
-        })}
-    })
+      // calling the World Bank API to set region & income level:
+      this.fetchWBCountryInfo(country).subscribe((wbData: any) => {
+
+        const countryInfo: any = {};
+
+        // append the GeoNames API data
+        countryInfo.country = geoData.geonames[0]?.countryName || 'No Data';
+        countryInfo.capital = 'Capital: ' + (geoData.geonames[0]?.capital || 'No Data');
+        countryInfo.population = 'Population: ' + (geoData.geonames[0]?.population || 'No Data');
+        countryInfo.continent = 'Continent: ' + (geoData.geonames[0]?.continentName || 'No Data');
+        countryInfo.currency = 'Currency: ' + (geoData.geonames[0]?.currencyCode || 'No Data')
+
+        // append the World Bank API data
+        countryInfo.region = 'Region: ' + wbData[1][0]?.region.value || 'No Data';
+        countryInfo.income = 'Income: ' + wbData[1][0]?.incomeLevel.value || 'No Data';
+
+        subject.next(countryInfo);
+        subject.complete();
+      });
+    });
 
     return subject.asObservable();
   }
--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-world-map',
@@ -8,18 +10,26 @@ import { ApiService } from '../api.service';
   templateUrl: './world-map.component.html',
   styleUrl: './world-map.component.css'
 })
+
 export class WorldMapComponent {
 
   countryInfo: any = {};
+  private hoverSubject = new Subject<string>();
 
-  constructor(private apiService: ApiService) {  }
-
-  setCountryData(event: any) {
-    this.apiService.setCountryInfo(event.target.id).subscribe((data: any) => {
-      this.countryInfo = {
-        ...data
-      }
+  constructor(private apiService: ApiService) {
+    // prevent the user from making too many api calls:
+    this.hoverSubject.pipe(
+      debounceTime(300) // wait for 300 milliseconds before making the call
+    ).subscribe((country: string) => {
+      // set the country info via api service:
+      this.apiService.setCountryInfo(country).subscribe((data: any) => {
+        this.countryInfo = { ...data };
+      });
     });
   }
-  
+
+  setCountryData(event: any) {
+    const country = event.target.id;
+    this.hoverSubject.next(country);
+  }
 }
